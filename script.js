65b0e4644b7156ca26a943e337a1bb7fa1ab15bd
Brock Biever <brock.biever10@gmail.com>
2024-05-06 22:54:05 -0500

    incomplete

--- script.js
+++ script.js
@@ -15,6 +15,74 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
+document.getElementById('hp-meter') = HP ;
+document.getElementById('ap-meter') = AP ;
+
+function arcSep(){
+    HP - 14 ;
+AP - 12 ;
+} ;
+
+function ent(){
+    HP - 9 ;
+    AP - 23 ;
+}
+ ;
+ function drag(){
+    AP - 38 ;
+    HP - 47 ;
+ } ;
+ function Star(){
+    AP - 38 ;
+    Hp - 47 ;
+ } ;
+
+ if(HP <= 0){
+    let dead = document.getElementsByClassName("freaky-fungus walk");
+    dead.classList
+ } ;
+ if(AP <= 0){
+    let outAP = document.getElementsByClassName("freaky-fungus jump");
+    outAP.classList
+    let buttons = document.querySelectorAll("input[type=button]");
+    buttons.disable ;
+ } ;
+/* <div class="container">
+<img src="./images/forest-bg-dk.png" alt="forest bg" class="img-bg">
+
+<div class="attacks">
+    <div class="ap-text">100 AP</div>
+    <progress id="ap-meter" value="100" max="100"></progress>
+    <button class="attack-btn arcane-scepter"></button>
+    <button class="attack-btn entangle"></button>
+    <button class="attack-btn dragon-blade"></button>
+    <button class="attack-btn star-fire"></button>
+</div>
+
+<div class="enemy">
+    <div class="hp-text">100 HP</div>
+    <progress id="hp-meter" value="100" max="100"></progress>
+    <div class="freaky-fungus walk"></div> */
+//     ## Attacks
+
+// ### ![arcane scepter](./images/arcane-scepter.png) Arcane Scepter
+
+// - **AP Cost:** 12
+// - **HP Damage:** 14
 
 
-onReady()
+// ### ![entangle](./images/entangle-3.png) Entangle
+
+// - **AP Cost:** 23
+// - **HP Damage:** 9
+
+// ### ![dragon blade](./images/dragon-blade-3.png) Dragon Blade
+
+// - **AP Cost:** 38
+// - **HP Damage:** 47
+
+// ### ![star fire](./images/star-fire.png) Star Fire
+
+// - **AP Cost:** 33
+// - **HP Damage:** 25
+
